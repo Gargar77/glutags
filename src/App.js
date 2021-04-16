497c3257a9abec9de33b7dd7d6e6e6eb778d653f
gargar77 <gbautista2@knights.ucf.edu>
2021-04-16 10:43:06 -0400

    implemented user DB storage flow

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Auth from './pages/auth/auth.component';
 import Header from './components/header/header.component';
-import { auth } from './firebase/firebase.utils';
+import { auth,createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor() {
@@ -25,10 +25,21 @@ class App extends React.Component {
   is signed in at any given time. always remember to close subscriptions on unmount*/
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser:user});
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
+        userRef.onSnapshot(snapshot => {
+          this.setState({
+            currentUser:{
+              id:snapshot.id,
+              ...snapshot.data()
+            }
+          },() => console.log(this.state))
+        });
+      } else {
+        this.setState({currentUser: userAuth})
+      }
     })
   }
 
